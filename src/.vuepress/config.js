08de43d03cdaea7a1be47d3235f3bae6b42a0d9e
Samuel Thomas <samuelthomas@Samuels-MBP.lan>
2021-01-20 17:17:06 +0800

    pushing draft one

---
+++ src/.vuepress/config.js
@@ -0,0 +1,89 @@
+const { description } = require('../../package')
+base: "/vuepressdocs/",
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'API Docs',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  theme: "craftdocs",
+    themeConfig: {
+      codeLanguages: {
+        go: "Golang",
+        javascript: "Javascript",
+        python: "Python",
+        ruby: "Ruby",
+        yaml: "YAML",
+        json: "JSON",
+        xml: "XML",
+      },
+    },
+    markdown: {
+      anchor: { level: [2, 3] },
+      extendMarkdown(md) {
+        let markup = require("vuepress-theme-craftdocs/markup");
+        md.use(markup);
+      },
+    },
+  
+  
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      },
+      {
+        text: 'VuePress',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
