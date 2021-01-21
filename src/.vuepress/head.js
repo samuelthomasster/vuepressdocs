e947e360427906aa58e81ffbc8582b116e81ecce
Samuel Thomas <samuelthomas@Samuels-MacBook-Pro.local>
2021-01-21 21:44:46 +0800

    Fixed header info

--- src/.vuepress/head.js
+++ src/.vuepress/head.js
@@ -7,28 +7,14 @@ module.exports = [
       htmlElement.className += (htmlElement.className ? ' ' : '') + 'theme-' + localStorage['docs.theme'];
     }`
   ],
-  [
-    "link",
-    {
-      rel: "icon",
-      href: "https://docs.craftcms.com/siteicons/favicon-16x16.png"
-    }
-  ],
-  [
-    "link",
-    {
-      rel: "apple-touch-icon",
-      sizes: "180x180",
-      href: "https://docs.craftcms.com/siteicons/apple-touch-icon.png"
-    }
-  ],
+  
   [
     "link",
     {
       rel: "icon",
       type: "image/png",
       sizes: "32x32",
-      href: "https://docs.craftcms.com/siteicons/favicon-32x32.png"
+      href: "./favicon32.png"
     }
   ],
   [
@@ -37,23 +23,7 @@ module.exports = [
       rel: "icon",
       type: "image/png",
       sizes: "16x16",
-      href: "https://docs.craftcms.com/siteicons/favicon-16x16.png"
-    }
-  ],
-  [
-    "link",
-    {
-      rel: "mask-icon",
-      href: "https://docs.craftcms.com/siteicons/safari-pinned-tab.svg",
-      color: "#e5422b"
-    }
-  ],
-  ["meta", { name: "msapplication-TileColor", content: "#f1f5fd" }],
-  [
-    "meta",
-    {
-      name: "msapplication-config",
-      content: "https://docs.craftcms.com/browserconfig.xml"
+      href: "./favicon16.png"
     }
   ],
   ["meta", { name: "theme-color", content: "#f1f5fd" }]
